9f6db0216603f01161660fc7b057af6d5ba81a8c
Michael <adeyelurem@gmail.com>
2026-09-06 15:46:02 +0100

    Quantities for body defined

---
+++ include/Physics/World.h
@@ -0,0 +1,10 @@
+//
+// Created by Michael Adeyelure on 06/09/2026.
+//
+
+#ifndef OMINOUS_ORBITS_WORLD_H
+#define OMINOUS_ORBITS_WORLD_H
+
+#include "Body.h"
+
+#endif //OMINOUS_ORBITS_WORLD_H
---
+++ src/World.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michael Adeyelure on 06/09/2026.
+//
+
+#include "Physics/World.h"
