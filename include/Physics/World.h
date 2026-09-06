637994bdaceecc4ebb1dd3eeb7f2336b8845ddc9
Michael <adeyelurem@gmail.com>
2026-09-06 16:11:22 +0100

    World as a body factory

--- include/Physics/World.h
+++ include/Physics/World.h
@@ -5,6 +5,22 @@
 #ifndef OMINOUS_ORBITS_WORLD_H
 #define OMINOUS_ORBITS_WORLD_H
 
+#include <cstddef>
+#include <vector>
+
 #include "Body.h"
+
+class World {
+public:
+    std::size_t create_body(const Vector3 &position, double mass);
+    void destroy_body(std::size_t index);
+
+    Body& get_body(const std::size_t index){return bodies[index];}
+    std::vector<Body>& get_bodies(){return bodies;}
+
+private:
+    std::vector<Body> bodies;
+    std::vector<std::size_t> free_indices;
+};
 
 #endif //OMINOUS_ORBITS_WORLD_H
--- src/World.cpp
+++ src/World.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Physics/World.h"
+
+std::size_t World::create_body(const Vector3 &position, const double mass) {
+    Body body;
+    body.position = position;
+    body.mass = mass;
+
+    if (!free_indices.empty()) {
+        const std::size_t target = free_indices.back();
+        free_indices.pop_back();
+
+        bodies[target] = body;
+
+        return target;
+    }
+
+    bodies.push_back(body);
+
+    return bodies.size() - 1;
+}
+
+
+void World::destroy_body(const std::size_t index) {
+    if (!bodies[index].is_alive) return;
+
+    bodies[index].is_alive = false;
+    free_indices.push_back(index);
+}
